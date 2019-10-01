5dc566972e0761a930f6f881b5693db15f02e3f2
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-10-01 18:12:00 +0200

    load the data into DB

--- database_setup.py
+++ database_setup.py
@@ -43,6 +43,7 @@ def connect_database(query):
     try:
         con = psycopg2.connect(dbname="exchange_rates")
         cur = con.cursor()
+        print(query)
         cur.execute(query)
         con.commit()
         print("The query has been excuted")
@@ -53,15 +54,40 @@ def connect_database(query):
     else:
         con.close()
 
+
+def insert_value(postgres_insert_query, record_to_insert):
+    try:
+        connection = psycopg2.connect(dbname="exchange_rates")
+        cursor = connection.cursor()
+
+        cursor.execute(postgres_insert_query, record_to_insert)
+
+        connection.commit()
+        count = cursor.rowcount
+        print (count, "Record inserted successfully into rate table")
+
+    except (Exception, psycopg2.Error) as error :
+        if(connection):
+            print("Failed to insert record into mobile table", error)
+
+    finally:
+        #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+
 def create_exchange_table():
     '''Create a table '''
-    create_db()
+    # create_db()
     # unpack the header into the table values
-    createTable = '''CREATE TABLE EXCHANGE(
+    createTable = '''CREATE TABLE RATE(
                   id serial primary key,
                   date text,
-                  source varchar(3),
-                  target varchar(3));'''
+                  source text,
+                  target text,
+                  value numeric);'''
     # connect to the database and run the query
     connect_database(createTable)
 
--- exchange_data.py
+++ exchange_data.py
@@ -1,12 +1,28 @@
 import httplib2
 import json
+from database_setup import insert_value
+import psycopg2
+
 
 def getExchangeData():
 
-    # url = "https://api.exchangeratesapi.io/history?start_at=2019-01-01&end_at=2019-10-01&base=USD"
-    url = "https://api.exchangeratesapi.io/history?start_at=2019-01-01&end_at=2019-10-01&symbols=USD"
+    url_usd = "https://api.exchangeratesapi.io/history?start_at=2019-01-01&end_at=2019-10-01&base=USD&symbols=EUR,GBP"
+    url_eur = "https://api.exchangeratesapi.io/history?start_at=2019-01-01&end_at=2019-10-01&symbols=USD,GBP"
+    url_gbp = "https://api.exchangeratesapi.io/history?start_at=2019-01-01&end_at=2019-10-01&base=GBP&symbols=USD,EUR"
     h = httplib2.Http()
-    result = json.loads(h.request(url, 'GET')[1])
-    return result['rates']
 
-print(getExchangeData())
+    result_usd = json.loads(h.request(url_usd, 'GET')[1])
+    result_eur = json.loads(h.request(url_eur, 'GET')[1])
+    result_gbp = json.loads(h.request(url_gbp, 'GET')[1])
+    parse_dict(result_eur, result_usd, result_gbp)
+
+
+def parse_dict(*dict):
+    data = ((i[0], some_dict['base'], j, i[1][j]) for some_dict in dict for i in some_dict['rates'].items() for j in i[1].keys())
+    for i in data:
+        insert_value(""" INSERT INTO RATE (date, source, target, value) VALUES (%s,%s,%s,%s)""",i)
+
+
+
+if __name__ == '__main__':
+    getExchangeData()
