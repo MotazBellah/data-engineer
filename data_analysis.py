d45aa43ffa4bf69362e4de5d3c61c6ee2641845a
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-10-09 01:37:16 +0200

    Update query on data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,9 +1,9 @@
 from database_setup import get_data
 
 def run():
-    return get_data('''select date, target from rate where date = '2019-06-06'
-                    and source = 'USD' union  select date, target from rate
-                    where date = '2019-06-07' and source = 'EUR' ;''')
+    return get_data('''select date, source, target, value from rate where
+                    date = '2019-06-06' and source = 'USD' union  select date, source,
+                    target, value from rate where date = '2019-06-07' and source = 'EUR' ;''')
 
 if __name__ == '__main__':
     print(run())
